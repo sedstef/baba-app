196c5ee13f14ef4b0d3adeb13fbfa1c621d27ec6
Stefan Sedelmaier <stefan@sedelmaier.at>
2025-12-28 15:31:08 +0100

    Dump banner text on start

--- src/main/java/at/fhj/softsec/baba/Main.java
+++ src/main/java/at/fhj/softsec/baba/Main.java
@@ -1,7 +1,23 @@
 package at.fhj.softsec.baba;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Baba!");
+
+        try {
+            String bannerText = readBannerText();
+            System.out.println(bannerText);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String readBannerText() throws IOException{
+        try (InputStream is = Main.class.getResourceAsStream("/banner.txt")) {
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        }
     }
 }
