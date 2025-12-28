bfe6cb4a15aca683cb2153fac6d783ee23d2fe9c
Stefan Sedelmaier <stefan@sedelmaier.at>
2025-12-28 16:53:43 +0100

    Add test for register command

--- src/test/java/at/fhj/softsec/baba/ZionTest.java
+++ src/test/java/at/fhj/softsec/baba/ZionTest.java
@@ -1,9 +1,11 @@
 package at.fhj.softsec.baba;
 
 import org.junit.Test;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -24,6 +26,24 @@ public class ZionTest {
         //assert
         String output = baos.toString(StandardCharsets.UTF_8);
         String printPrompt = "Baba$: Available commands: help, exit\nBaba$: ";
+        assertThat(output, is(printPrompt));
+    }
+
+    @Test
+    public void promptLoopParsesRegister() throws Exception {
+        // arrange
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(baos, true, StandardCharsets.UTF_8);
+
+        BufferedReader reader = CommandReader.of("register", "user", "password", "password", "exit");
+        Zion zion = new Zion(out, reader);
+
+        // act
+        zion.promptLoop();
+
+        //assert
+        String output = baos.toString(StandardCharsets.UTF_8);
+        String printPrompt = "Baba$: username: password: verify password: Baba$: ";
         assertThat(output, is(printPrompt));
     }
 }
